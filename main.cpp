94d8b61c6871cce20051c23f00f4b3903ef1c0b5
IliyanGavrilov <wardheromgrtoxic@gmail.com>
2023-12-13 15:44:47 +0200

    Input ships onto map and small code improvements

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ enum Mode {Singleplayer = 's', Multiplayer = 'm'};
 enum Difficulty {Easy = 1, Medium = 2, Hard = 3, Impossible = 4};
 enum Randomness {Normal = 'n', Rigged = 'y'};
 enum Placement {CreateCustom = 1, LoadFromFile = 2, Random = 3};
-enum TileState {Water = '~', Unhit = '~', Hit = 'X', Miss = 'O', Sunken = '*'};
+enum TileState {Water = '~', Unhit = '!', Hit = 'X', Miss = 'O', Sunken = '*'};
 
 struct point_t {
   int x;
@@ -18,70 +18,72 @@ struct point_t {
 };
 
 struct ship_t {
+  int size;
   point_t end_coords[2];
 };
 
 // Check input
-bool invalid_input(int size, Mode mode, Placement ship_placement, Difficulty difficulty, Randomness randomness) {
+bool validate_input(int size, Mode mode, Placement ship_placement, Difficulty difficulty, Randomness randomness) {
   // Check map size
   if(size < MIN_SIZE || size > MAX_SIZE) {
-    return true;
+    return false;
   }
 
   // Check game mode
   if(mode != Mode::Singleplayer && mode != Mode::Multiplayer) {
-    return true;
+    return false;
   }
 
   // Check difficulty of bot
   if(difficulty != Difficulty::Easy && difficulty != Difficulty::Medium && difficulty != Difficulty::Hard && difficulty != Difficulty::Impossible) {
-    return true;
+    return false;
   }
 
   // Check if bot can cheat/if randomness is rigged
   if(randomness != Randomness::Normal && randomness != Randomness::Rigged) {
-    return true;
+    return false;
   }
 
   // Check ship placement
   if(ship_placement != Placement::CreateCustom && ship_placement != Placement::LoadFromFile && ship_placement != Placement::Random) {
-    return true;
+    return false;
   }
 
-  return false;
+  return true;
 }
 
-void fix_start_end_coords(int &x1, int &x2, int &y1, int &y2) {
-  if(x1 > x2 || y1 > y2) {
-    swap(x1, x2);
-    swap(y1, y2);
+void fix_start_end_coords(point_t &p1, point_t &p2) {
+  if(p1.x > p2.x || p1.y > p2.y) {
+    swap(p1.x, p2.x);
+    swap(p1.y, p2.y);
   }
 }
 
 // TODO "const" TileState ** const map, const int size, const ship_t end_points
-bool valid_coords(TileState ** map, int map_size, int ship_size, int x1, int x2, int y1, int y2) {
+// TODO space between for () if () while () etc.
+bool valid_coords(TileState ** map, int map_size, int ship_size, point_t p1, point_t p2) {
   // Coordinates are outside of map
-  if(x1 < 0 || x1 >= map_size || x2 < 0 || x2 >= map_size || y1 < 0 || y1 >= map_size || y2 < 0 || y2 >= map_size) {
-    cout << "Invalid coordinates - Given coordinates are bigger than map size!\n";
+  if(p1.x < 0 || p1.x >= map_size || p2.x < 0 || p2.x >= map_size || p1.y < 0 || p1.y >= map_size || p2.y < 0 || p2.y >= map_size) {
+    cout << "Invalid coordinates - Given coordinates are outside of the map!\n";
     return false;
   }
 
   // Coordinates not on same row/column
-  if(x1 != x2 && y1 != y2) {
+  if(p1.x != p2.x && p1.y != p2.y) {
     cout << "Invalid coordinates - Given coordinates not lying on same row/column!\n";
     return false;
   }
 
   // Vertical ship
-  if(x1 == x2) {
+  if(p1.x == p2.x) {
     // If given coordinates can fit ship perfectly
-    if(y2 - y1 + 1 != ship_size) {
+    if(p2.y - p1.y + 1 != ship_size) {
       cout << "Invalid coordinates - Given coordinates do not match ship size!\n";
       return false;
     }
 
-    for(int i = y1; i <= y2; i++) {
-      if(map[i][x1] != TileState::Water) {
+    for(int i = p1.y; i <= p2.y; i++) {
+      if(map[i][p1.x] != TileState::Water) {
         cout << "Invalid coordinates - There is a ship lying between the given points!\n";
         return false;
       }
@@ -90,13 +92,13 @@ bool valid_coords(TileState ** map, int map_size, int ship_size, int x1, int x2,
   // Horizontal ship (y1 == y2)
   else {
     // If given coordinates can fit ship perfectly
-    if(x2 - x1 + 1 != ship_size) {
+    if(p2.x - p1.x + 1 != ship_size) {
       cout << "Invalid coordinates - Given coordinates do not match ship size!\n";
       return false;
     }
 
-    for(int i = x1; i <= x2; i++) {
-      if(map[y1][i] != TileState::Water) {
+    for(int i = p1.x; i <= p2.x; i++) {
+      if(map[p1.y][i] != TileState::Water) {
         cout << "Invalid coordinates - There is a ship lying between the given points!\n";
         return false;
       }
@@ -106,8 +108,30 @@ bool valid_coords(TileState ** map, int map_size, int ship_size, int x1, int x2,
   return true;
 }
 
-void set_ship_coords_on_map(TileState **map, ship_t ships) {
+void set_ship_coords_on_map(TileState **map, ship_t ship) {
+  point_t p1 = ship.end_coords[0], p2 = ship.end_coords[1];
 
+  // Vertical ship
+  if(p1.x == p2.x) {
+    for(int i = p1.y; i <= p2.y; i++) {
+      map[i][p1.x] = TileState::Unhit;
+    }
+  }
+    // Horizontal ship (y1 == y2)
+  else {
+    for(int i = p1.x; i <= p2.x; i++) {
+      map[p1.y][i] = TileState::Unhit;
+    }
+  }
+}
+
+void print_map(TileState **map, int size) {
+  for(int i = size - 1; i >= 0; i--) {
+    for(int j = 0; j < size; j++) {
+      cout << (char)map[i][j] << " ";
+    }
+    cout << "\n";
+  }
 }
 
 TileState **create_custom_map(int size, int *ships_to_place, ship_t *ships) {
@@ -120,14 +144,14 @@ TileState **create_custom_map(int size, int *ships_to_place, ship_t *ships) {
   }
 
   for(int i = 0; i < 5;) {
-    int x1, y1, x2, y2;
+    point_t p1, p2;
     cout << "Place ship with size " << ships_to_place[i] << "(x1 y1 x2 y2)\n";
-    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> p1.x >> p1.y >> p2.x >> p2.y;
 
-    fix_start_end_coords(x1, x2, y1, y2);
+    fix_start_end_coords(p1, p2);
 
-    if(valid_coords(map, size, ships_to_place[i], x1, x2, y1, y2)) {
-      ships[i] = ship_t{point_t{x1, y1}, point_t{x2, y2}};
+    if(valid_coords(map, size, ships_to_place[i], p1, p2)) {
+      ships[i] = ship_t{ships_to_place[i], p1, p2};
       set_ship_coords_on_map(map, ships[i]);
       i++;
     }
@@ -136,7 +160,7 @@ TileState **create_custom_map(int size, int *ships_to_place, ship_t *ships) {
   return map;
 }
 
-void generate_map(int size, Mode mode, Placement ship_placement) {
+TileState **generate_map(int size, Placement ship_placement) {
   TileState **map = nullptr;
   int ships_to_place[5] = {5, 4, 3, 3, 2};
   ship_t ships[5];
@@ -152,51 +176,73 @@ void generate_map(int size, Mode mode, Placement ship_placement) {
 
     }break;
   }
+
+  // print_map(map, size); // TODO TEMP
+  // Clear terminal so other player doesn't see coordinates entered
+  // system("@cls||clear");; // TODO ASK IF ALLOWED
+
+  return map;
+}
+
+void game_loop(TileState **map1, TileState **map2) {
+
 }
 
 void start_game() {
-// Get input as int or char
-  int map_size, iDifficulty;
-  char cMode, cRandomness, cPlacement;
+  // Get input as int or char
+  int map_size, iDifficulty, iPlacement;
+  char cMode, cRandomness;
   Difficulty eDifficulty = Difficulty::Easy;
   Randomness eRandomness = Randomness::Normal;
 
+  cout << "New game (1) or load game from file (2)? \n"; // TODO
+
   // Map size
-  cout << "Field size (5, 20):";
+  cout << "Field size (5, 20): ";
   cin >> map_size;
 
   // Game mode
-  cout << "Singleplayer(S) or multiplayer(M)?";
+  cout << "Singleplayer(S) or multiplayer(M)? ";
   cin >> cMode;
   Mode eMode = static_cast<Mode>(tolower(cMode));
 
   // Singleplayer computer options
   if(eMode == Mode::Singleplayer) {
     // Difficulty
-    cout << "Select difficulty: 1. Easy 2. Medium 3. Hard 4. Impossible";
+    cout << "Select difficulty: 1. Easy 2. Medium 3. Hard 4. Impossible ";
     cin >> iDifficulty;
     eDifficulty = static_cast<Difficulty>(iDifficulty);
-    if(eDifficulty != Difficulty::Impossible)
-    // Rigged computer randomness
-    cout << "Can computer *cheat*? (y, n)";
-    cin >> cRandomness;
-    eRandomness = static_cast<Randomness>(cRandomness);
+    if(eDifficulty != Difficulty::Impossible) {
+      // Rigged computer randomness
+      cout << "Can computer *cheat*? (y, n) ";
+      cin >> cRandomness;
+      eRandomness = static_cast<Randomness>(cRandomness);
+    }
   }
 
   // Ship placement
-  cout << "Create custom(1), load from file (2) or random placement(3)?";
-  cin >> cPlacement;
-  Placement ePlacement = static_cast<Placement>(cPlacement);
+  cout << "Create custom(1), load from file (2) or random placement(3)? ";
+  cin >> iPlacement;
+  Placement ePlacement = static_cast<Placement>(iPlacement);
 
   cout << "\n";
   cout << "";
 
-  if(!invalid_input(map_size, eMode, ePlacement, eDifficulty, eRandomness)) {
+  if(!validate_input(map_size, eMode, ePlacement, eDifficulty, eRandomness)) {
     cout << "Invalid input!\n";
     return;
   }
 
-  generate_map(map_size, eMode, ePlacement);
+  TileState **map1 = generate_map(map_size, ePlacement);
+  TileState **map2;
+  if(eMode == Mode::Singleplayer) {
+    map2 = generate_map(map_size, Placement::Random);
+  }
+  else {
+    map2 = generate_map(map_size, ePlacement);
+  }
+
+  game_loop(map1, map2);
 }
 
 int main() {
