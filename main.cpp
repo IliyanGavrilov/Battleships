059fc8345e97c66bbce42078aa37d33d6e22f99e
IliyanGavrilov <wardheromgrtoxic@gmail.com>
2023-12-14 23:37:28 +0200

    Add more input options, refactor code, fix warnings, free memory

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+enum GameType {New = 1, Load = 2};
+enum SuccessfulHit {RepeatTurn = 'y', SwitchTurn = 'n'};
 enum Mode {Singleplayer = 's', Multiplayer = 'm'};
 enum Difficulty {Easy = 1, Medium = 2, Hard = 3, Impossible = 4};
 enum Randomness {Normal = 'n', Rigged = 'y'};
@@ -23,7 +25,17 @@ struct ship_t {
 };
 
 // Check input
-bool validate_input(int size, Mode mode, Placement ship_placement, Difficulty difficulty, Randomness randomness) {
+bool validate_input(int size, GameType gameType, SuccessfulHit successfulHit, Mode mode, Difficulty difficulty, Randomness randomness) {
+  // Check game type
+  if(gameType != GameType::Load && gameType != GameType::New) {
+    return false;
+  }
+
+  // Check to repeat turn or not after a successful ship hit
+  if(successfulHit != SuccessfulHit::RepeatTurn && successfulHit != SuccessfulHit::SwitchTurn) {
+    return false;
+  }
+
   // Check map size
   if(size < MIN_SIZE || size > MAX_SIZE) {
     return false;
@@ -44,6 +56,10 @@ bool validate_input(int size, Mode mode, Placement ship_placement, Difficulty di
     return false;
   }
 
+  return true;
+}
+
+bool validate_ship_placement(Placement ship_placement) {
   // Check ship placement
   if(ship_placement != Placement::CreateCustom && ship_placement != Placement::LoadFromFile && ship_placement != Placement::Random) {
     return false;
@@ -144,9 +160,33 @@ TileState **create_custom_map(int size, int *ships_to_place, ship_t *ships) {
   }
 
   for(int i = 0; i < 5;) {
-    point_t p1, p2;
+    point_t p1 = {}, p2 = {};
     cout << "Place ship with size " << ships_to_place[i] << "(x1 y1 x2 y2)\n";
     cin >> p1.x >> p1.y >> p2.x >> p2.y;
+
+    fix_start_end_coords(p1, p2);
+
+    if(valid_coords(map, size, ships_to_place[i], p1, p2)) {
+      ships[i] = ship_t{ships_to_place[i], p1, p2};
+      set_ship_coords_on_map(map, ships[i]);
+      i++;
+    }
+  }
+
+  return map;
+}
+
+TileState **create_random_map(int size, int *ships_to_place, ship_t *ships) {
+  TileState **map = new TileState *[size];
+  for(int i = 0; i < size; i++) {
+    map[i] = new TileState[size];
+    for(int j = 0; j < size; j++) {
+      map[i][j] = TileState::Water;
+    }
+  }
+
+  for(int i = 0; i < 5;) {
+    point_t p1 = {}, p2 = {};
 
     fix_start_end_coords(p1, p2);
 
@@ -170,10 +210,10 @@ TileState **generate_map(int size, Placement ship_placement) {
       map = create_custom_map(size, ships_to_place, ships);
     }break;
     case Placement::LoadFromFile: {
-
+      //map = load_map_from_file();
     }break;
     case Placement::Random: {
-
+      map = create_random_map(size, ships_to_place, ships);
     }break;
   }
 
@@ -188,14 +228,68 @@ void game_loop(TileState **map1, TileState **map2) {
 
 }
 
-void start_game() {
-  // Get input as int or char
-  int map_size, iDifficulty, iPlacement;
-  char cMode, cRandomness;
+Placement get_placement() {
+  int iPlacement;
+  Placement ePlacement;
+  do {
+    // Ship placement
+    cout << "Create custom(1), load from file (2) or random placement(3)? ";
+    cin >> iPlacement;
+    ePlacement = static_cast<Placement>(iPlacement);
+  } while(!validate_ship_placement(ePlacement));
+
+  return ePlacement;
+}
+
+void free_map_memory(TileState **map, int size) {
+  // Free each column (sub-array)
+  for(int i = 0; i < size; i++) {
+    delete[] map[i];
+  }
+
+  // Free each row (array of pointers)
+  delete[] map;
+  map = nullptr;
+}
+
+void start_game(int map_size, GameType gameType, SuccessfulHit successfulHit, Mode mode, Difficulty difficulty, Randomness randomness) {
+  // Create both maps
+  Placement placement = get_placement();
+  TileState **map1 = generate_map(map_size, placement);
+
+  TileState **map2;
+  if(mode == Mode::Singleplayer) {
+    map2 = generate_map(map_size, Placement::Random);
+  }
+  else {
+    placement = get_placement();
+    map2 = generate_map(map_size, placement);
+  }
+
+  cout << "\n";
+
+  // Start game
+  game_loop(map1, map2);
+
+  free_map_memory(map1, map_size);
+  free_map_memory(map2, map_size);
+}
+
+int main() {
+  // Get input as int or char to convert it to enum afterwards
+  int map_size, iDifficulty, iGameType;
+  char cMode, cRandomness, cSuccessfulHit;
+
   Difficulty eDifficulty = Difficulty::Easy;
   Randomness eRandomness = Randomness::Normal;
 
   cout << "New game (1) or load game from file (2)? \n"; // TODO
+  cin >> iGameType;
+  GameType eGameType = static_cast<GameType>(iGameType);
+
+  cout << "Does player go again after a successful hit? (y, n) "; // TODO
+  cin >> cSuccessfulHit;
+  SuccessfulHit eSuccessfulHit = static_cast<SuccessfulHit>(tolower(cSuccessfulHit));
 
   // Map size
   cout << "Field size (5, 20): ";
@@ -212,6 +306,7 @@ void start_game() {
     cout << "Select difficulty: 1. Easy 2. Medium 3. Hard 4. Impossible ";
     cin >> iDifficulty;
     eDifficulty = static_cast<Difficulty>(iDifficulty);
+
     if(eDifficulty != Difficulty::Impossible) {
       // Rigged computer randomness
       cout << "Can computer *cheat*? (y, n) ";
@@ -220,33 +315,12 @@ void start_game() {
     }
   }
 
-  // Ship placement
-  cout << "Create custom(1), load from file (2) or random placement(3)? ";
-  cin >> iPlacement;
-  Placement ePlacement = static_cast<Placement>(iPlacement);
-
-  cout << "\n";
-  cout << "";
-
-  if(!validate_input(map_size, eMode, ePlacement, eDifficulty, eRandomness)) {
+  if(!validate_input(map_size, eGameType, eSuccessfulHit, eMode, eDifficulty, eRandomness)) {
     cout << "Invalid input!\n";
-    return;
+    return -1;
   }
 
-  TileState **map1 = generate_map(map_size, ePlacement);
-  TileState **map2;
-  if(eMode == Mode::Singleplayer) {
-    map2 = generate_map(map_size, Placement::Random);
-  }
-  else {
-    map2 = generate_map(map_size, ePlacement);
-  }
-
-  game_loop(map1, map2);
-}
-
-int main() {
-  start_game();
+  start_game(map_size, eGameType, eSuccessfulHit, eMode, eDifficulty, eRandomness);
 
   return 0;
 }
